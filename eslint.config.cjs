49f3f29275d93bba018b9e39a17c1f2d98990733
Moksha K <110051904+Moksha-Kachhia@users.noreply.github.com>
2025-10-04 02:41:44 -0400

    updated config file.

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,9 +1,8 @@
-// eslint.config.js
-import js from "@eslint/js";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
+const js = require("@eslint/js");
+const react = require("eslint-plugin-react");
+const reactHooks = require("eslint-plugin-react-hooks");
 
-export default [
+module.exports = [
   js.configs.recommended, // base JS rules
   {
     files: ["frontend/**/*.js", "frontend/**/*.jsx", "frontend/**/*.ts", "frontend/**/*.tsx"],
